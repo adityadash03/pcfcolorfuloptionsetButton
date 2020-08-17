7cbd18cb230a47de7823f9b451c894a0c93b2ad1
Aditya Dash <69818451+adityadash03@users.noreply.github.com>
2020-08-17 16:28:18 -0400

    Add files via upload

---
+++ ColorfulOptionSetButton/generated/ManifestTypes.d.ts
@@ -0,0 +1,11 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    OptionSetAttribute: ComponentFramework.PropertyTypes.OptionSetProperty;
+}
+export interface IOutputs {
+    OptionSetAttribute?: number;
+}
